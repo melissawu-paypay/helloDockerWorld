1155f6e419adc30404b61a274ed79832c0b49127
Melissa Wu <t-melissa.wu@paypay-corp.co.jp>
2020-06-22 09:28:44 +0800

    base project

--- src/main/resources/db/migration/V1_1__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `mydata` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `dttm` DATETIME NOT NULL,
-    `value` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`)
-);
--- src/main/resources/db/migration/V1_2__rename-dockerworlddata.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE mydata RENAME dockerworlddata;
